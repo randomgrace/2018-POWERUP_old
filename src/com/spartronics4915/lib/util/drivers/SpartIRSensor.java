447493ba5d97a6eeae296ee83aaa3150bbfaed04
Randy Groves <randy.groves@boeing.com>
2018-02-11 16:22:26 -0800

    Start addition of model arrays

--- src/com/spartronics4915/lib/util/drivers/SpartIRSensor.java
+++ src/com/spartronics4915/lib/util/drivers/SpartIRSensor.java
@@ -1,5 +1,8 @@
 package com.spartronics4915.lib.util.drivers;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 import com.spartronics4915.lib.util.drivers.IRSensor;
 
 /**
@@ -11,8 +14,16 @@ public class SpartIRSensor
 
     protected final double mMinVoltage = 0.25;
     protected final double mMaxVoltage = 3.1;
-	private static int kIRSensorPort = 3;
-	private IRSensor mSensor = new IRSensor(kIRSensorPort, mMinVoltage, mMaxVoltage);
+    private static int kIRSensorPort = 3;
+    private IRSensor mSensor = new IRSensor(kIRSensorPort, mMinVoltage, mMaxVoltage);
+    Map<String, double[]> models = new TreeMap<String, double[]>();
+
+    models.put("A41", new double[] {0.25, 3.1, 4.0, 30.0});
+    models.put("A51", new double[] {0.25, 5.5, 2.0, 15.0});
+    models.put("A21", new double[] {0.25, 5.5, 2.0, 15.0});
+    models.put("A60", new double[] {0.25, 5.5, 2.0, 15.0});
+    models.put("A02", new double[] {0.25, 5.5, 2.0, 15.0});
+    models.put("A71", new double[] {0.25, 5.5, 2.0, 15.0});
     
 
     public SpartIRSensor(int port)
@@ -31,11 +42,11 @@ public class SpartIRSensor
 
     public boolean isTargetAcquired(double voltage)
     {
-		if (voltage > mMinVoltage && voltage < mMaxVoltage) {
-			return true;
-		} else {
-			return false;
-		}
+        if (voltage > mMinVoltage && voltage < mMaxVoltage) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
